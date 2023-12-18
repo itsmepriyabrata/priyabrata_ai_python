24b9d8bee57bbaa038eb83ef938dd0856f5552f1
itsmepriyabrata <153970432+itsmepriyabrata@users.noreply.github.com>
2023-12-18 18:43:39 +0530

    Update neuron.py

--- neuron.py
+++ neuron.py
@@ -1,36 +1,34 @@
 import numpy as np
 
-# Sigmoid activation function
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+class Neuron:
+    def _init_(self, input_size):
+        # Initialize weights and bias with random values
+        self.weights = np.random.rand(input_size)
+        self.bias = np.random.rand()
+    
+    def sigmoid(self, x):
+        """Sigmoid activation function."""
+        return 1 / (1 + np.exp(-x))
+    
+    def forward(self, inputs):
+        """Perform a forward pass through the neuron."""
+        weighted_sum = np.dot(inputs, self.weights) + self.bias
+        activation = self.sigmoid(weighted_sum)
+        return activation
 
-# Neural network parameters
-input_size = 3  # Number of input features
-output_size = 1  # Number of neurons in the output layer
+# Example usage
+if _name_ == "_main_":
+    # Create a neuron with 3 input connections
+    neuron = Neuron(input_size=3)
 
-# Initialize weights and bias
-weights = np.random.rand(input_size, output_size)
-bias = np.random.rand(1)
+    # Sample input data
+    input_data = np.array([0.5, 0.3, 0.8])
 
-# Input data
-input_data = np.array([[0.2, 0.4, 0.7]])
+    # Perform a forward pass through the neuron
+    output = neuron.forward(input_data)
 
-# Forward pass
-weighted_sum = np.dot(input_data, weights) + bias
-output = sigmoid(weighted_sum)
-
-# Display results
-print("Input Data:")
-print(input_data)
-
-print("\nWeights:")
-print(weights)
-
-print("\nBias:")
-print(bias)
-
-print("\nWeighted Sum:")
-print(weighted_sum)
-
-print("\nOutput:")
-print(output)
+    # Print the results
+    print("Input Data:")
+    print(input_data)
+    print("\nOutput:")
+    print(output)
