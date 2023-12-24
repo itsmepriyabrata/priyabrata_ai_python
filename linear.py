8b105f468b29637d02c07104801f9f8e80b2a29d
itsmepriyabrata <153970432+itsmepriyabrata@users.noreply.github.com>
2023-12-24 18:41:03 +0530

    Create linear.py

---
+++ linear.py
@@ -0,0 +1,11 @@
+def linear_equation(x, m, b):
+    y = m * x + b
+    return y
+
+# Example usage:
+x_value = 5
+slope = 2
+intercept = 3
+
+result = linear_equation(x_value, slope, intercept)
+print(f"For x = {x_value}, the y value is: {result}")
