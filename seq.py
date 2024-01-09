33ad205d8977e70600bd1a3f22e7d439258cb197
itsmepriyabrata <153970432+itsmepriyabrata@users.noreply.github.com>
2024-01-09 18:58:28 +0530

    Create seq.py

---
+++ seq.py
@@ -0,0 +1,19 @@
+def sequential_operations(num1, num2):
+    # Operation 1: Addition
+    result_addition = num1 + num2
+    print(f"Sequential Addition Result: {result_addition}")
+
+    # Operation 2: Multiplication
+    result_multiplication = num1 * num2
+    print(f"Sequential Multiplication Result: {result_multiplication}")
+
+if __name__ == "__main__":
+
+
+
+    # Input
+      num1 = int(input("Enter the first integer: "))
+      num2 = int(input("Enter the second integer: "))
+
+    # Sequential Execution
+      sequential_operations(num1, num2)
