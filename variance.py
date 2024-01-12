b6ff1c6020c8f18f2175ab48e35ff3cf59221e6a
itsmepriyabrata <153970432+itsmepriyabrata@users.noreply.github.com>
2024-01-12 16:35:08 +0530

    Create variance.py

---
+++ variance.py
@@ -0,0 +1,11 @@
+def calculate_variance(data):
+       n=len(data)
+       mean=sum(data)/n
+       squared_diff=[(x-mean)**2 for x in data]
+       variance=sum(squared_diff)/n
+       return variance
+
+data=[2,3,6,8,10]
+result=calculate_variance(data)
+print(f"the variance of the data is:{result} ")
+            
