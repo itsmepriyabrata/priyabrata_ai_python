5a2eebea5372ab92faf3ab85db87243f9c984cfc
itsmepriyabrata <153970432+itsmepriyabrata@users.noreply.github.com>
2024-01-19 18:38:52 +0530

    Create txt.py

---
+++ txt.py
@@ -0,0 +1,8 @@
+def append_name_to_file(C:\Users\priyabrata\Desktop\scrapping\odia.txt, name):
+    with open(C:\Users\priyabrata\Desktop\scrapping\odia.txt, 'a') as file:
+        file.write('\n' + name)
+
+# Example usage
+existing_file_path = 'odia.txt'
+your_name = 'Your Name'
+append_name_to_file(C:\Users\priyabrata\Desktop\scrapping\odia.txt, your_name)
