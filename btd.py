d6270e2c66aa7e14642bb36ddc666fedf7beb0b3
itsmepriyabrata <153970432+itsmepriyabrata@users.noreply.github.com>
2024-01-21 13:15:13 +0530

    Create btd.py

---
+++ btd.py
@@ -0,0 +1,7 @@
+def binary_to_decimal(binary):
+      decimal=int(binary,2)
+      return decimal
+
+binary="1101"
+decimal_result=binary_to_decimal(binary)
+print(f"The decimal equivalent of {binary} is {decimal_result}")
