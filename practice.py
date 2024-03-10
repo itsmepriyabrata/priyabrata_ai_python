db8b5af995c050589e53a04235d39c74a6089fb7
itsmepriyabrata <153970432+itsmepriyabrata@users.noreply.github.com>
2024-03-10 20:21:07 +0530

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,39 @@
+# def all_unique(numbers):
+     
+#      if len(numbers)==len(set(numbers)):
+#           return True
+#      else:
+#           return False
+# print(all_unique([1,2,33,44,5]))
+# print(all_unique([3,5,6,8]))
+
+     
+# import random
+# char_list=['a','e','i','o','u']
+# random.shuffle(char_list)
+# print(''.join(char_list))
+# result=[]
+# for num in range(1500,2701):
+#     if num%7==0 and num%5==0:
+#         result.append(num)
+# print("Numbers divisible by 7 and multiples of 5 betwwen 1500 and 2700:")
+# print(result)
+
+# def celsius_to_fahrenheit(celsius):
+#     fahrenheit=(celsius * 9/5) +32
+#     return fahrenheit
+# def fahrenheit_to_celsius(fahrenheit):
+#     celsius=(fahrenheit-32)*5/9
+#     return celsius
+# celsius_value=25
+# fahrenheit_value=77
+# print(f"{celsius_value} degrees celsius is equal to{celsius_to_fahrenheit(celsius_value)} degrees fahrenheit")
+# print(f"{fahrenheit_value} degrees fahrenheit is equal to {fahrenheit_to_celsius(fahrenheit_value)} degrees celsius")
+
+# import random
+# random_number=random.randint(1,9)
+# guess=int(input("guess a number between 1 and 9:"))
+# if guess==random_number:
+#     print("congratulations!you guess the correct number ")
+# else:
+#     print(f"sorry, the correct number was {random_number}.Try agin!")
