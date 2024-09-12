7d9282703b6f7de288ee10c7fa21a0fe0ad37e67
itsmepriyabrata <153970432+itsmepriyabrata@users.noreply.github.com>
2024-09-13 00:13:13 +0530

    Create BackgroundApp.js

---
+++ BackgroundApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'C:\Users\priyabrata\Downloads\Recorder_Front-main\Recorder_Front-main\public\subtle-cinematics-acZd3gWgfOw-unsplash.jpg';  // Link to the updated CSS file
+
+function BackgroundApp() {
+  return (
+    <div className="background">
+      <h1>Welcome to My Website</h1>
+      <p>This is an example of a React webpage with a background image.</p>
+    </div>
+  );
+}
+
+export default BackgroundApp;
